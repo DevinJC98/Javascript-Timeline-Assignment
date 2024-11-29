550a51a1685e3a07e43cbc4b1adf99813cc66bd0
DevinJC98 <devinjc98@hotmail.com>
2024-11-28 20:14:52 -0700

    slight updates all around

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let stringcheck = "1";
 
 
 const light = document.getElementById("lightMode");
-const dark = document.getElementById("darkMode");
+
 
 light.addEventListener("click", function()
 {
@@ -72,7 +72,7 @@ y1.addEventListener("click", function()
 
         pic.src = "Images/littlegoober.jpg";
         title.innerHTML = "Poutine's First Year";
-        text.innerHTML = "Poutine \"Middle Name Redacted\" Campbell was born on July 1st, 2020. Because she was born on canada and I am a chef, the only logical name I could give her was poutine. In her first year, poutine was a little fluffy ball of chaos. This was first evident when she jumped directly into the toilet bowl as a kitten in the first month I had her.";
+        text.innerHTML = "Poutine \"Middle Name Redacted\" Campbell was born on July 1st, 2020. Because she was born on Canada Day and I am a chef, the only logical name I could give her was Poutine. In her first year, Poutine was a little fluffy ball of chaos. This was first evident when she jumped directly into the toilet bowl as a kitten within a month of me having her.";
        } 
 
        if(y1.classList.contains("lightOffButton"))
@@ -97,7 +97,7 @@ y2.addEventListener("click", function()
 
         pic.src = "Images/2yrold.jpg";
         title.innerHTML = "Poutine's Second Year";
-        text.innerHTML = "Poutine quickly grew from a cute, vicious baby to a cute, vicious two year old. As she grew in age, so did she grow in her hunger. Specifically for pieces of my couch. while incredibly annoying, poutine was also growing more comfortable living in my apartment and she quickly began to wrestle control of my home from me."
+        text.innerHTML = "Poutine quickly grew from a cute, vicious baby, to a cute, vicious two year old. As she grew in age, so did she grow hunger. Specifically, hunger for pieces of my couch. While incredibly annoying, Poutine was also growing more comfortable living in my apartment and she quickly began to wrestle control of my home away from me."
     }
     
     if(y2.classList.contains("lightOffButton"))
@@ -122,7 +122,7 @@ y3.addEventListener("click", function()
 
         pic.src = "Images/blep.jpg";
         title.innerHTML = "Poutine's Third year";
-        text.innerHTML = "At the tender age of two and a bit, poutine had began calming down slightly. That being said, she still claimed her territory with an iron fist. Her middle of the night yells, and sharp clawed stabbings were only rivalled by her desire to fall asleep and wake herself up by stretching. This always caused her to stick out her tongue, which is the cutest thing she does."
+        text.innerHTML = "At the tender age of two and a bit, Poutine had began calming down slightly. That being said, she still ruled her territory with an iron fist. Her middle of the night yells, and sharp clawed stabbings were only rivalled by her desire to fall asleep and wake herself up by stretching. This always caused her to stick out her tongue, which is the cutest thing she does."
     } 
 
     if(y3.classList.contains("lightOffButton"))
@@ -148,7 +148,7 @@ y4.addEventListener("click", function()
 
         pic.src = "Images/sleepypout.jpg";
         title.innerHTML = "Present Day Poutine";
-        text.innerHTML = "This brings us to 2024. Poutine still yells at three in the morning, yet she spends most of her time asleep in the closet. When she does decide to sleep at night, her perch is right next to my head, smacking my face with her tail until she dozes off. Poutine is now 4 years old, and I look forward to many more years of hanging out with my fuzzy little pal."
+        text.innerHTML = "This brings us to 2024. Poutine still yells at three in the morning, yet she spends most of her time asleep in the closet. When she does decide to sleep at night, her perch is right next to my head, the perfect place to smack my face with her tail until she dozes off. Poutine is now 4 years old, and I look forward to many more years of hanging out with my fuzzy little pal."
     } 
     
     if(y4.classList.contains("lightOffButton"))
